3f0a715a6d239f46ab9457beed3c7a1509686d37
jayanthdevendran-ctrl <jayanthdevendran@gmail.com>
2026-07-27 11:46:21 +0530

    moved arrows

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { Reflector } from 'three/examples/jsm/Addons.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const raycaster = new THREE.Raycaster();
@@ -7,16 +8,16 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const spotlight = new THREE.SpotLight (0xffffff, 100, 20, 1, 2);
-spotlight.penumbra = 0.8
-spotlight.position.set(0, 5, 1);
-spotlight.target.position.set(0, 1, -5);
+const spotlight = new THREE.SpotLight (0xffffff, 100, 10, 2.5, 1);
+spotlight.penumbra = 1
+spotlight.position.set(0, 6, 3);
+spotlight.target.position.set(0, 0.5, -5);
 scene.add(spotlight.target);
 scene.add(spotlight);
 
-const models = ['/models/bmwe46.glb',
-   '/models/clkgtr.glb',
-  '/models/supra.glb']
+const models = [{file : '/models/bmwe46.glb', scale : 2},
+  {file:'/models/clkgtr.glb', scale: 2},
+  {file:''}]
 const cars = []
 const modelname = []
 
@@ -36,8 +37,21 @@ const leftButton = new THREE.Mesh(
     })
 );
 
+const mirror = new Reflector(
+    new THREE.CircleGeometry(10),
+    {
+        color: 0x303030,
+        textureWidth: window.InnerWidth,
+        textureHeight: window.InnerHeight
+    }
+)
+mirror.position.y = 0;
+mirror.rotateX(-Math.PI / 2);
+scene.add(mirror);
+
+
 leftButton.name = "Left";
-leftButton.position.set(-4, 0, -5);
+leftButton.position.set(-7, 5, 0);
 
 scene.add(leftButton);
 
@@ -50,16 +64,16 @@ const rightButton = new THREE.Mesh(
 );
 
 rightButton.name = "Right";
-rightButton.position.set(4, 0, -3);
+rightButton.position.set(7, 5, 0);
 
 scene.add(rightButton);
 
 models.forEach((file, index) => {
-  loader.load(file, function(gltf)
+  loader.load(file.file, function(gltf)
   {
     const car = gltf.scene;
-    car.scale.set(2, 2, 2);
-    car.position.set(index * 10, 0.5, -3);
+    car.scale.setScalar(file.scale);
+    car.position.set(index * 11, 0.5, -3);
     scene.add(car);
     cars.push(car)
   });
@@ -68,7 +82,7 @@ models.forEach((file, index) => {
 function next() {
   cars.forEach(car =>
     {
-      car.position.x = car.position.x - 8;
+      car.position.x = car.position.x - 11;
     }
   );
   currentcar = (currentcar + 1) % cars.length;
@@ -77,7 +91,7 @@ function next() {
 function previous() {
     cars.forEach(car =>
     {
-      car.position.x = car.position.x + 8;
+      car.position.x = car.position.x + 11;
     }
   );
   currentcar = (currentcar - 1 + cars.length) % cars.length;
@@ -105,8 +119,8 @@ window.addEventListener("click", (event) => {
 }
 );
 
-camera.position.y = 3;
-camera.position.z = 7;
+camera.position.y = 5;
+camera.position.z = 10;
 
 function animate() {
   requestAnimationFrame(animate);
