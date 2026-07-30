d26cc5a37add24b1f31db8e311d3841ad89454eb
jayanthdevendran-ctrl <jayanthdevendran@gmail.com>
2026-07-30 16:29:49 +0530

    final

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { Reflector } from 'three/addons/objects/Reflector.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,23 +8,39 @@ camera.position.set(0, 2, 6);
 camera.lookAt(0, 1, 0);
 const loader = new GLTFLoader();
 
+
 window.addEventListener("resize", () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 //lightsgohere
 const ambient = new THREE.AmbientLight(0xffffff, 1);
 scene.add (ambient);
 const directed = new THREE.DirectionalLight(0xffffff, 1);
 directed.position.set(5, 5, 5);
 scene.add (directed);
+//addreflectorlater
+const floorGeometry = new THREE.PlaneGeometry(200,200);
 
+const floor = new Reflector(floorGeometry,{
+    clipBias:0.003,
+    textureWidth:window.innerWidth * window.devicePixelRatio,
+    textureHeight:window.innerHeight * window.devicePixelRatio,
+    color:0x777777
+});
+
+floor.rotation.x = -Math.PI/2;
+floor.position.y = -2.2;
+
+scene.add(floor);
 const models = [
     {
         file: "/models/bmwe46.glb",
-        scale: 2,
+        scale: 1,
         name: "BMW M3 E46",
         descriptions: "One of the most iconic M cars ever, a need for speed icon that was the last naturally aspirated M3.",
         specs: {
@@ -36,8 +53,8 @@ const models = [
         }
     },
     {
-        file: "/models/clk.glb",
-        scale: 2,
+        file: "/models/clkgtr.glb",
+        scale: 1,
         name: "Mercedes CLK GTR",
         descriptions: "A homologation special from the FIA GT championship, this car took off more than its designers meant for it to",
         specs: {
@@ -49,19 +66,19 @@ const models = [
             weight: "1000 kg"
         }
 
-    }
+    },
     {
         file: "/models/mclarenlh.glb",
         scale: 100,
-        name: "Mercedes CLK GTR",
-        descriptions: "A homologation special from the FIA GT championship, this car took off more than its designers meant for it to",
+        name: "McLaren MP4-23",
+        descriptions: "Lewis Hamilton's 2008 championship winning car, known for brazil 2008's dramatic race.",
         specs: {
-            engine: "6.9L V12",
-            power: "622 bhp",
-            topspeed: "346km/h",
-            zerotohundred: "3.8s",
+            engine: "2.4L V8",
+            power: "755 bhp",
+            topspeed: "354 km/h",
+            zerotohundred: "2.3s",
             drivetrain: "RWD",
-            weight: "1000 kg"
+            weight: "605 kg"
         }
 
     }
@@ -91,8 +108,8 @@ models.forEach((car, i) => {
             const model = gltf.scene;
             model.scale.setScalar(car.scale);
             model.position.x = (i*7)
-            model.position.y = 0;
-            model.position.z = 0;
+            model.position.y = -2;
+            model.position.z = 1;
             scene.add(model);
             carmodels[i] = model;
             if(i == 0)
@@ -112,32 +129,40 @@ let currentcar = 0;
 let right = false;
 let left = false;
 
+camera.position.y = 0;
+
 document.getElementById("next").onclick = () => {
     if(moving) return;
+
     currentcar++;
-    if(currentcar >= models.length){
-        currentcar = 0;
-        camera.position.x = 0;
-    }
+if(currentcar >= models.length){
+    currentcar = 0;
+    camera.position.x = 0;
+    targetcar = 0;
+    moving = false;
+}
+else{
     targetcar = currentcar * 7;
     moving = true;
-    right = true;
+}
     updateInfo();
-
 };
 
 document.getElementById("previous").onclick = () => {
     if(moving) return;
+
     currentcar--;
-    if(currentcar < 0){
-        currentcar = models.length - 1;
-        camera.position.x = currentcar * 7;
-    }
+if(currentcar < 0){
+    currentcar = models.length - 1;
+    camera.position.x = currentcar * 7;
+    targetcar = camera.position.x;
+    moving = false;
+}
+else{
     targetcar = currentcar * 7;
     moving = true;
-    left = true;
+}
     updateInfo();
-
 };
 
 
@@ -160,7 +185,27 @@ else if(camera.position.x > targetcar){
         moving = false;
     }
 }
+carmodels.forEach((car) => {
+    if(car){
+        car.rotation.y += 0.005;
+    }
+});
 renderer.render( scene, camera );
 
 }
+function add_star() {
+  const star_geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const star_material = new THREE.MeshBasicMaterial({ color: 0xffffff  });
+  const star = new THREE.Mesh(star_geometry, star_material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(200));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(add_star);
+
 animate();
